267e53058e879005a07bca829e1671ced8542141
David Rasch <David.Rasch@cwi.nl>
2025-08-18 14:15:57 +0200

    stuff for android builds

---
+++ android/app/src/main/kotlin/nl/cloudscripting/tessellation/MainActivity.kt
@@ -0,0 +1,5 @@
+package nl.cloudscripting.tessellation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/nl/hacklab/tessellationapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package nl.hacklab.tessellationapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
