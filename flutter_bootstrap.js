02ce056edd306ba9a9c11df87721f595d0e69a9e
dcrasch <dcrasch@users.noreply.github.com>
2025-08-26 07:53:11 +0000

    deploy: 185b83bc2efad92bd460bb2ba4497377a980fd9c

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1928223877"
+    serviceWorkerVersion: "2663135682"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "37900473a47c8099e635657e4c3a524e",
+const RESOURCES = {"flutter_bootstrap.js": "13627334b8e64874abf159fc4770f5ee",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "af7cb69811d0c4cff4bb6337a52ae1ea",
 "icons/Icon-maskable-512.png": "bb3272e85606baa2db9a4ede0a230594",
